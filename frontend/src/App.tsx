45f83b313021e9a0bdda2564ebfe860e741491ce
Sani Radanlieva <radanlievas@gmail.com>
2024-08-31 16:37:45 +0200

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css'; // Make sure this file includes some basic styling for your app
 import MapComponent from './MapComponent';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to TrailVista</h1>
+      <Header />
       </header>
       <main>
         <div>
-          <MapComponent />
+          {/* <MapComponent /> */}
+          <Footer />
         </div>
       </main>
     </div>
